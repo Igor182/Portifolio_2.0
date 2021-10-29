1a5525037aa7a54dc79f208e751fcd446429d034
Igor Mello <igormello_2014@hotmail.com>
2021-10-28 22:21:08 -0300

    Mais um commit, dessa vez com um script pronto pra calcular o valor do trabalho em tempo real e já mostrando pra o usuário, futuramente atualizarei mais para deixa-lo mais completo

--- script.js
+++ script.js
@@ -1,30 +1,70 @@
-var hamburguer = document.querySelector(".hamgurguer");
+var hamburguer = document.querySelector(".hamburguer");
 
 document.querySelector(".hamburguer").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
 );
+    
+     
 
-document.querySelector("#qtde").addEventListener("change", atualizarPreco)
-document.querySelector("#js").addEventListener("change", atualizarPreco)
-document.querySelector("#layout-sim").addEventListener("change", atualizarPreco)
-document.querySelector("#layout-nao").addEventListener("change", atualizarPreco)
-document.querySelector("#prazo").addEventListener("change", function () {
-    const prazo = document.querySelector("#prazo").value
-    document.querySelector("label[for=prazo]").innerHTML = `Prazo: ${prazo} semanas`
-    atualizarPreco()
-})
+function clicar(){
+    let paginas = document.getElementById('quanti').value;
+    let caixa = document.getElementById('js')
+    let prazzo = document.getElementById('prazo').value;
+    let result = document.getElementById('valor');
 
-function atualizarPreco(){
-    const qtde = document.querySelector("#qtde").value
-    const temJS = document.querySelector("#js").checked
-    const incluiLayout = document.querySelector("#layout-sim").checked
-    const prazo = document.querySelector("#prazo").value
-  
-    let preco = qtde * 100;
-    if (temJS) preco *= 1.1
-    if (incluiLayout) preco += 500
-    let taxaUrgencia = 1 - prazo*0.1;
-    preco *= 1 + taxaUrgencia
-   
-    document.querySelector("#preco").innerHTML = `R$ ${preco.toFixed(2)}`
+    let calcpagg = (paginas * 500)
+    
+    let calcpagcaixa = (calcpagg * 1.25)
+    let calcpagprazo = ''
+    
+
+    
+
+    if (prazzo == 1 && caixa.checked){
+        result.innerHTML = `<p>R$ ${calcpagcaixa * 1.60}</p>`
+} else if (prazzo == 2 && caixa.checked) {
+        result.innerHTML = `<p>R$ ${calcpagcaixa * 1.55}</p>`
+} else if (prazzo == 3 && caixa.checked) {
+    result.innerHTML = `<p>R$ ${calcpagcaixa * 1.50}</p>`
+} else if (prazzo == 4 && caixa.checked) {
+    result.innerHTML = `<p>R$ ${calcpagcaixa * 1.45}</p>`
+} else if (prazzo == 5 && caixa.checked) {
+    result.innerHTML = `<p>R$ ${calcpagcaixa * 1.40}</p>`
+} else if (prazzo == 6 && caixa.checked) {
+    result.innerHTML = `<p>R$ ${calcpagcaixa * 1.35}</p>`
+} else if (prazzo == 7 && caixa.checked) {
+    result.innerHTML = `<p>R$ ${calcpagcaixa * 1.30}</p>`
+} else if (prazzo == 8 && caixa.checked) {
+    result.innerHTML = `<p>R$ ${calcpagcaixa * 1.25}</p>`
+} else if (prazzo == 9 && caixa.checked) {
+    result.innerHTML = `<p>R$ ${calcpagcaixa * 1.20}</p>`
+} else if (prazzo == 10 && caixa.checked) {
+    result.innerHTML = `<p>R$ ${calcpagcaixa * 1.15}</p>`
+} else if (prazzo == 1){
+    result.innerHTML = `<p>R$ ${calcpagcaixa * 1.60}</p>`
+} else if (prazzo == 2) {
+    result.innerHTML = `<p>R$ ${calcpagcaixa * 1.55}</p>`
+} else if (prazzo == 3) {
+result.innerHTML = `<p>R$ ${calcpagcaixa * 1.50}</p>`
+} else if (prazzo == 4) {
+result.innerHTML = `<p>R$ ${calcpagcaixa * 1.45}</p>`
+} else if (prazzo == 5) {
+result.innerHTML = `<p>R$ ${calcpagcaixa * 1.40}</p>`
+} else if (prazzo == 6) {
+result.innerHTML = `<p>R$ ${calcpagcaixa * 1.35}</p>`
+} else if (prazzo == 7) {
+result.innerHTML = `<p>R$ ${calcpagcaixa * 1.30}</p>`
+} else if (prazzo == 8) {
+result.innerHTML = `<p>R$ ${calcpagcaixa * 1.25}</p>`
+} else if (prazzo == 9) {
+result.innerHTML = `<p>R$ ${calcpagcaixa * 1.20}</p>`
+} else if (prazzo == 10) {
+result.innerHTML = `<p>R$ ${calcpagcaixa * 1.15}</p>`
+} 
+
+
+    
+        
 }
+
+
